7485b453567a42948a36c87ee0db7976cf768ba9
Fauzan Radji <triputrafauzan@gmail.com>
2023-08-04 02:04:24 +0800

    fix: meta description, robots.txt, tap targets

--- src/Components/NavLink.jsx
+++ src/Components/NavLink.jsx
@@ -1,12 +1,12 @@
-import PropTypes from "prop-types";
 import { NavLink as Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export default function NavLink({ to, children }) {
   return (
     <Link
       to={to}
       className={({ isActive }) =>
-        `flex flex-1 items-center justify-center gap-x-2 rounded-md px-3 py-2 font-medium md:flex-none ${
+        `flex flex-1 items-center justify-center gap-x-2 rounded-md px-3 py-3 font-medium md:flex-none md:py-2 ${
           isActive
             ? "bg-complimentary text-complimentaryContrast"
             : "text-neutralContrast hover:bg-complimentary/50 hover:text-complimentaryContrast"
